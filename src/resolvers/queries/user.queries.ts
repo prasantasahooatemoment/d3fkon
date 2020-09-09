1995d8e683ee47314423e1996615c39213e04550
Nikhil Sridhar <nikhil@boilerplate.in>
2020-09-10 01:14:57 +0530

    testing user Query

--- src/resolvers/queries/user.queries.ts
+++ src/resolvers/queries/user.queries.ts
@@ -5,7 +5,7 @@ export const userQuery = extendType({
   type: 'Query',
   definition(t) {
     t.crud.user({"filtering":true, "ordering":true, "alias":"user", computedInputs: {
-      where: ({args, ctx})=> ({user: {id: ctx.userId}})
+      where: ({args, ctx})=> ({id: ctx.userId})
     }})
 	t.crud.users({"filtering":true, "ordering":true, "alias":"users"})
   }
