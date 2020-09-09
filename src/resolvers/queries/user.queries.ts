17d418671f7821b34c1325fb30c290d6a4b87b74
Nikhil Sridhar <nikhil@boilerplate.in>
2020-09-10 01:11:15 +0530

    testing user Query

--- src/resolvers/queries/user.queries.ts
+++ src/resolvers/queries/user.queries.ts
@@ -4,9 +4,7 @@ import { extendType } from '@nexus/schema'
 export const userQuery = extendType({
   type: 'Query',
   definition(t) {
-    t.crud.user({"filtering":true, "ordering":true, "alias":"user", computedInputs:{
-      id:({ctx, args})=>ctx.userId
-    }})
+    t.crud.user({"filtering":true, "ordering":true, "alias":"user"})
 	t.crud.users({"filtering":true, "ordering":true, "alias":"users"})
   }
 })
--- src/schema.ts
+++ src/schema.ts
@@ -6,7 +6,12 @@ import { nexusSchemaPrisma } from 'nexus-plugin-prisma/schema'
 
 export const schema = makeSchema({
   types: [allTypes],
-  plugins: [nexusSchemaPrisma({experimentalCRUD:true, prismaClient: (ctx:Context) => ctx.prisma},)],
+  plugins: [nexusSchemaPrisma({experimentalCRUD:true, prismaClient: (ctx:Context) => ctx.prisma, 
+  computedInputs:{
+    user:({args, ctx, info}) => ({ connect: { where: { id: ctx.userId } } })
+  }
+
+})],
   outputs: {
     schema: __dirname + '/../schema.graphql',
     typegen: __dirname + '/generated/nexus.ts',
