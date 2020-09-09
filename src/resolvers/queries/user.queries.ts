430291ea7ba7141864649065cfe132e9b3982424
Nikhil Sridhar <nikhil@boilerplate.in>
2020-09-10 01:21:50 +0530

    testing user Query

--- src/resolvers/queries/user.queries.ts
+++ src/resolvers/queries/user.queries.ts
@@ -3,10 +3,18 @@ import { extendType } from '@nexus/schema'
 export const userQuery = extendType({
   type: 'Query',
   definition(t) {
-    t.crud.user({ filtering: true, ordering: true, alias: 'user', resolve: async(root, args, ctx, info, original) => {
-      args.where = {id: ctx.userId};
-      return original(root, args, ctx, info)
-    }})
+    t.crud.user({
+      alias: 'user',
+      computedInputs:{
+        where: ({args, ctx, info})=> ({
+          id:{equals:ctx.userId}
+        })
+      }
+      // resolve: async (root, args, ctx, info, original) => {
+      //   args.where = { id: ctx.userId }
+      //   return original(root, args, ctx, info)
+      // },
+    })
     t.crud.users({ filtering: true, ordering: true, alias: 'users' })
   },
 })
