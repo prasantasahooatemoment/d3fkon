503a71e5f1bc6bcfd42d94dec87663ee8aad56ba
Nikhil Sridhar <nikhil@boilerplate.in>
2020-09-10 03:07:12 +0530

    Testing updateOneuser

--- src/resolvers/mutations/user.mutations.ts
+++ src/resolvers/mutations/user.mutations.ts
@@ -10,7 +10,7 @@ export const userMutations = extendType({
     
     t.crud.updateOneuser({
       computedInputs: {
-        name: ({ctx, args}) => {
+        id: ({ctx, args}) => {
           return ctx.userId
         }
       }
